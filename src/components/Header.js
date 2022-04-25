34e58e991dd23c3b2d7051cb631d8fb514d3633e
Hamimi Zouaoui <zouaoui.hm@gmail.com>
2022-04-25 18:17:36 +0200

    cv changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ function Header() {
                 <nav className="header--nav">
                     <ul className="nav--list">
                         <li><a href="https://www.linkedin.com/in/hamimi-zouaoui/" target="_blank" rel='noreferrer'>LinkedIn</a></li>
-                        <li><a href={require("../images/CV_zouaoui_hamimi_developpeur_web-alternance.pdf")} target="_blank" rel='noreferrer' >Mon CV</a></li>
+                        <li><a href={require("../images/CV_zouaoui_hamimi_developpeur_web_alternance.pdf")} target="_blank" rel='noreferrer' >Mon CV</a></li>
                         <li><a href="https://github.com/Korgmgn" target="_blank" rel='noreferrer'>Github</a></li>
                         <li><a href="#contact">Me contacter</a></li>
                     </ul>
